926dd48cb02d5c62c72970d007a3a9336171ff46
Luke Sweeney <luke@thesweeneys.org>
2017-11-15 00:11:16 -0600

    Makes buffer work

--- controller.js
+++ controller.js
@@ -11,17 +11,26 @@ var app = new Vue({
   methods: {
     setTempBars() {
       document.querySelector('#pvBar').addEventListener('mdl-componentupgraded', function () {
-        this.MaterialProgress.setProgress(app.pid.pv / 2);
+        this.MaterialProgress.setProgress(app.pid.pv / 2.12);
+        if (app.pid.pid_running) {
+          this.MaterialProgress.setBuffer(87);
+        }
       });
       document.querySelector('#svBar').addEventListener('mdl-componentupgraded', function () {
-        this.MaterialProgress.setProgress(app.pid.sv / 2);
+        this.MaterialProgress.setProgress(app.pid.sv / 2.12);
       });
       return true;
     },
 
     updateTempBars() {
-      document.querySelector('#pvBar').MaterialProgress.setProgress(this.pid.pv / 2);
-      document.querySelector('#svBar').MaterialProgress.setProgress(this.pid.sv / 2);
+      document.querySelector('#pvBar').MaterialProgress.setProgress(this.pid.pv / 2.12);
+      document.querySelector('#svBar').MaterialProgress.setProgress(this.pid.sv / 2.12);
+      if (this.pid.pid_running) {
+        document.querySelector('#pvBar').MaterialProgress.setBuffer(87);
+      } else {
+        document.querySelector('#pvBar').MaterialProgress.setBuffer(100);
+      }
+      return true;
     }
   },
 
