70fe86196e51096d9b2124db71c2ba8465570b18
Luke Sweeney <luke@thesweeneys.org>
2017-11-15 01:38:11 -0600

    Makes the timer work

--- controller.js
+++ controller.js
@@ -7,7 +7,9 @@ var app = new Vue({
       "sv": 157
     },
     timer: 0,
-    timerString: "Done"
+    timerInput: null,
+    timerUnit: "minutes",
+    timeString: "Done."
   },
 
   methods: {
@@ -33,14 +35,52 @@ var app = new Vue({
         document.querySelector('#pvBar').MaterialProgress.setBuffer(100);
       }
       return true;
+    },
+
+    getInputTimeInSeconds() {
+
+      if (this.timerInput == '') {
+        return 600;
+      }
+
+      if (this.timerUnit == 'seconds') {
+        return parseInt(this.timerInput);
+      } else {
+        return parseInt(this.timerInput * 60);
+      }
+    },
+
+    setTimer() {
+      this.timer = this.getInputTimeInSeconds();
+      this.endTime = this.now() + this.timer;
+      this.timeRemaining = this.timer;
+      this.timerInput = "";
+
+      window.setInterval(() => {
+        this.timeRemaining = this.endTime - this.now();
+        this.setTimeString();
+      }, 1000);
+    },
+
+    now() {
+      return Math.floor(Date.now() / 1000)
+    },
+
+    // This could be a computed property, but they don't update how I expect them to.
+    setTimeString() {
+      if (this.timeRemaining < 0) {
+        this.timeString = "Done.";
+        return true;
+      }
+      seconds = this.timeRemaining % 60;
+      var formattedSeconds = ("0" + seconds).slice(-2);
+      minutes = Math.floor(this.timeRemaining / 60);
+
+      this.timeString = minutes.toString() + ":" + formattedSeconds.toString();
     }
+
   },
 
-  computed: {
-    timeRemaining: function () {
-      return this.timerString;
-    }
-  },
 
   mounted() {
     this.setTempBars();
@@ -52,14 +92,6 @@ var app = new Vue({
         this.updateTempBars();
       },
       deep: true
-    },
-    timer: function () {
-
-      seconds = this.timer % 60;
-      var formattedSeconds = ("0" + seconds).slice(-2);
-      minutes = Math.floor(this.timer / 60);
-
-      this.timerString = minutes.toString() + ":" + formattedSeconds.toString();
     }
   }
 })
